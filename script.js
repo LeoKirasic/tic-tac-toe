30536a2c99384dbf99f4bf9fab53b7e9924305a7
Leo Kirasic <kirasicleo@gmail.com>
2021-08-16 19:37:45 +0200

    Set up renderContent functionality

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const Gameboard = (() =>{
     const gameBoard = ['X', 'X', 'X',
                        'O', 'O', 'O',
                        'X', 'X', 'X'];
-    function renderContent() {
+    const renderContent = () => {
+        for (let i = 0; i < gameBoard.length; i++) {
+            const container = document.querySelector('#gameboard');
+            const box = document.createElement('div');
+            box.textContent = gameBoard[i];
+            box.classList
+            container.appendChild(box);
+        }
+    }
+    renderContent();
+    return {
         
     }
 })();
+
