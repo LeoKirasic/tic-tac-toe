dc6568718097cb3e92e9745ad574aad98dbf8537
Leo Kirasic <kirasicleo@gmail.com>
2021-08-24 00:18:29 +0200

    Finishing touches

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 const Gameboard = (() =>{
     let box;
+    let container;
     const gameBoard = ['', '', '',
                        '', '', '',
                        '', '', ''];
+    const eventHandler = function(e) {
+        if(e.target.textContent === '') {
+            e.target.textContent = alternateMark();
+            addMark(gameBoard, this.id);
+            checkWinner();
+            } else {
+               console.log('illegal move');
+            }
+    }
     const renderContent = () => {
         for (let i = 0; i < gameBoard.length; i++) {
-            const container = document.querySelector('#gameboard');
+            container = document.querySelector('#gameboard');
             box = document.createElement('div');
             box.id = i;
+            box.dataset.index = i;
             box.textContent = gameBoard[i];
             box.classList.add('box');
-            box.addEventListener('click', function(e) {
-                if(e.target.textContent === '') {
-                e.target.textContent = alternateMark();
-                addMark(gameBoard, this.id);
-                checkWinner();
-                } else {
-                   console.log('illegal move');
-                }
-            });
+            box.addEventListener('click', eventHandler);
             container.appendChild(box);
         }
     }
 
+    const newGame = document.getElementById('new-game');
+    const title = document.getElementById('title');
+    title.appendChild(newGame);
+    newGame.addEventListener('click', () => {
+    window.location.reload();
+    });
+    
     let counter = 0;
-
+    
     const alternateMark = () => {
         if (Number.isInteger(counter / 2)) {
             return 'X';
@@ -41,39 +51,60 @@ const Gameboard = (() =>{
             }
             counter++;
     }
+    const clearDom = () => {
+        container.textContent = '';
+        for (let i = 0; i < gameBoard.length; i++) {
+            container = document.querySelector('#gameboard');
+            box = document.createElement('div');
+            box.id = i;
+            box.dataset.index = i;
+            box.textContent = gameBoard[i];
+            box.classList.add('box');
+            container.appendChild(box);
+        }
+    }
+    const createWinnerDisplay = (mark) => {
+        const winnerDisplay = document.createElement('div');
+        winnerDisplay.classList.add('winner-display');
+        winnerDisplay.textContent = `${gameBoard[mark]} player is the winner!`;
+        title.appendChild(winnerDisplay);
+        clearDom();
+    }
     const checkWinner = () => {
              // Row checks
         if(gameBoard[0] === gameBoard [1] && gameBoard[0] === gameBoard[2] && gameBoard[0] !== '') {
-            alert('first row winner');
+            createWinnerDisplay(0);
         } else if(gameBoard[3] === gameBoard [4] && gameBoard[3] === gameBoard[5] && gameBoard[3] !== ''){
-            alert('second row winner');
+            createWinnerDisplay(4);
         } else if(gameBoard[6] === gameBoard [7] && gameBoard[6] === gameBoard[8] && gameBoard[6] !== ''){
-            alert('third row winner');
+            createWinnerDisplay(6);
             // Column checks
         } else if(gameBoard[0] === gameBoard [3] && gameBoard[0] === gameBoard[6] && gameBoard[0] !== ''){
-            alert('first column winner');
+            createWinnerDisplay(0);
         } else if(gameBoard[1] === gameBoard [4] && gameBoard[1] === gameBoard[7] && gameBoard[1] !== ''){
-            alert('second column winner');
+            createWinnerDisplay(1);
         } else if(gameBoard[2] === gameBoard [5] && gameBoard[2] === gameBoard[8] && gameBoard[2] !== ''){
-            alert('third column winner');
+            createWinnerDisplay(2);
             // Diagonal checks
         } else if(gameBoard[0] === gameBoard [4] && gameBoard[0] === gameBoard[8] && gameBoard[0] !== ''){
-            alert('first diagonal winner');
+            createWinnerDisplay(0);
         } else if(gameBoard[2] === gameBoard [4] && gameBoard[2] === gameBoard[6] && gameBoard[2] !== ''){
-            alert('second diagonal winner');
+            createWinnerDisplay(2);
             // Draw test
         } else {
             for(let i = 0; i < gameBoard.length; i++) {
                 if(gameBoard.every(x => x !== '')) {
-                    alert('draw');
+                    const bodyDiv = document.querySelector('body');
+                    const winnerDisplay = document.createElement('div');
+                    winnerDisplay.classList.add('winner-display');
+                    winnerDisplay.textContent = `Draw!`;
+                    bodyDiv.appendChild(winnerDisplay);
                     break;
                 } else {
-                    console.log('invalid');
+                    console.log('Game ongoing');
                 }
             }
         }
     }
     renderContent();
-    return {
-    }
 })();
